8541a360bdd26d97b7ff860fa1f249a1abac1875
Ryan Yang <javathon@gmail.com>
2017-09-21 14:41:20 -0700

    added js code to start working on canvas

---
+++ script.js
@@ -0,0 +1,15 @@
+/*jslint white: true */
+/*
+function game(){
+    gameArea().start;
+}
+*/
+window.onload = function(){
+    "use strict";
+    
+    var userScore;
+    var userLevel;
+    var gameArea = document.getElementById("gameCanvas");
+    var ctx = gameArea.getContext("2d");
+    ctx.fillText("Hello World", 50, 50);
+};
