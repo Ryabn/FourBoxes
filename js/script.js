9cefd9f2f834bec6a99bbc383e2e4e690e3bdd81
Ryan Yang <javathon@gmail.com>
2017-12-25 18:08:33 -0800

    basic outline

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*jslint white: true */
+
+//declare variables
+var userScore;
+var userLevel;
+
+window.onload = function(){
+    "use strict";
+    
+
+};
+
+function displayNewScore(){
+    
+}
+function displayNewLevel(){
+   
+}
