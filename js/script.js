2ca05f36824a8c0c271d8cb4e7b95062b45728a4
Ryan Yang <javathon@gmail.com>
2017-12-25 18:31:26 -0800

    previously written js functions

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var userScore;
 var userLevel;
 
 window.onload = function(){
-    "use strict";
+
     
 
 };
@@ -15,4 +15,22 @@ function displayNewScore(){
 }
 function displayNewLevel(){
    
+}
+
+function iGenerateSequence(iGeneratedArray, iArrElements){
+	iGeneratedArray = [];
+	for(loopVar = 0; loopVar <= iArrElements; loopVar++){
+		int iRandomNumber = Math.floor(Math.random() * 4);
+		iGeneratedArray.push(iRandomNumber);
+	}
+	return iGeneratedArray;
+}
+
+function buttonPressed(buttonNum){
+	int iGenerated = iGeneratedArray[iUserInput.length - 1];
+	bCorrect(buttonNum, iGenerated);
+}
+
+function bCorrect(iUserInput, iGenerated){
+	return (iUserInput === iGenerated);
 }
