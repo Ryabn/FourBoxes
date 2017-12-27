480a4b3d836c7a12da27463d6c306aca3b5933bc
Ryan Yang <javathon@gmail.com>
2017-12-26 20:21:58 -0800

    I'm honestly a god. Holy. I just wrote a solid 50 lines+ of code without testing any of the functions individually and it all worked on the first try. Like this will never happen again. The chaining functions work as I made it operate recursively and it doesn't seem to affect performance until it begins displayingmore than a couple hundred sequences.

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ var iRoundIndex = 0;
 var gameButtonsId = ['one', 'two', 'three', 'four'];
 var gameButtonsIdLen = gameButtonsId.length;
 
+//disable buttons on load
 function load(){
     disableGameButtons();
 }
-
 function disableGameButtons(){
     for(iLoopVar = 0; iLoopVar <= gameButtonsIdLen; iLoopVar++){
         document.getElementById(gameButtonsId[iLoopVar]).disabled = true;
@@ -20,25 +20,55 @@ function enableGameButtons(){
         document.getElementById(gameButtonsId[iLoopVar]).disabled = false;
     }
 }
-
-
-function displayButton(buttonId){
-    document.getElementById(buttonId).style.boxShadow = "0 0 10px red";
-    setTimeout(function(){
-        stopButton(buttonId);
-    }, 1000);
-    //later will replace time with integer so that the longer the sequence, the shorter the animation
-    
-}
-function stopButton(buttonId){
-    document.getElementById(buttonId).style.boxShadow = "0 0 10px blue";
-}
-
 function chainAnimations(iGeneratedArray){
+    var sArrayId = [];
+    sArrayId = translateNumToId(iGeneratedArray);
+    var iAnimationIndex = 0;
     
+    function displayButton(iAnimationIndex){
+        document.getElementById(sArrayId[iAnimationIndex]).style.boxShadow = "0 0 10px red";
+        setTimeout(function(){
+            stopButton(iAnimationIndex);
+        }, 1000);
+    }
+    function stopButton(iAnimationIndex){
+        document.getElementById(sArrayId[iAnimationIndex]).style.boxShadow = "none";
+        setTimeout(function(){
+            iAnimationIndex++;
+            if(iAnimationIndex < sArrayId.length){
+                displayButton(iAnimationIndex);
+            }
+        }, 500);
+        
+    }
+    displayButton(iAnimationIndex);
 }
-
+//generates array of id's based on generated array
+function translateNumToId(iGeneratedArray){
+    var sArrayId = [];
+    for(iLoopVar = 0; iLoopVar < iGeneratedArray.length; iLoopVar++){
+        switch(iGeneratedArray[iLoopVar]) {
+            case 0:
+                sArrayId.push('one');
+                break;
+            case 1:
+                sArrayId.push('two')
+                break;
+            case 2:
+                sArrayId.push('three')
+                break;
+            case 3:
+                sArrayId.push('four')
+                break;
+        } 
+    }
+    return sArrayId;
+}
+function test(){
+    chainAnimations(iGenerateSequence(3));
+}
 function startButton(){
+    
     document.getElementById("startButton").disabled = true;
     
     var iGeneratedArray =[];
@@ -60,13 +90,11 @@ function startButton(){
     document.getElementById("Button").disabled = false;
     disableGameButtons();
 }
-
 //retrieves user input
 function uInput(number){
     userInput = number;
     alert(number);
 }
-
 //given array and number of indexes returns a randomized array of that length
 function iGenerateSequence(iArrElements){
 	GeneratedArray = [];
@@ -76,7 +104,6 @@ function iGenerateSequence(iArrElements){
 	}
 	return GeneratedArray;
 }
-
 /*
 //assume it's start of a round
 function checkInput(){
@@ -89,7 +116,6 @@ function checkInput(){
             
         }
     }
-
 }
 */
 function bCorrect(iUserInput, iGenerated){
