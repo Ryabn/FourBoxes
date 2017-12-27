e3bc11204d17257a5f7396bc9846b75ada1e9106
Ryan Yang <javathon@gmail.com>
2017-12-26 18:14:17 -0800

    random integer function was disabling javascript functionality

--- js/script.js
+++ js/script.js
@@ -1,37 +1,14 @@
-/*jslint white: true */
-
-//declare variables
-var userScore;
-var userLevel;
-var iGeneratedArray =[];
-
-window.onload = function(){
-
-    
-
-};
-
-function displayNewScore(){
-    
-}
-function displayNewLevel(){
-   document.getElementById('level') = "Level: " + userLevel;
-}
 
 //given array and number of indexes returns a randomized array of that length
 function iGenerateSequence(iGeneratedArray, iArrElements){
 	iGeneratedArray = [];
 	for(loopVar = 0; loopVar <= iArrElements; loopVar++){
-		int iRandomNumber = Math.floor(Math.random() * 4);
+		//int iRandomNumber = Math.floor(Math.random() * 4);
 		iGeneratedArray.push(iRandomNumber);
 	}
 	return iGeneratedArray;
 }
 
-function buttonPressed(buttonNum){
-	int iGenerated = iGeneratedArray[iUserInput.length - 1];
-	bCorrect(buttonNum, iGenerated);
-}
 
 function bCorrect(iUserInput, iGenerated){
 	return (iUserInput === iGenerated);
